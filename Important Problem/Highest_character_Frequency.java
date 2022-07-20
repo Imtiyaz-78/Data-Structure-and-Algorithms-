ef4ac9445af913222e622b7202c70d58088b883b
Imtiyazdeveloper <imtiyazmd803@gmail.com>
2022-07-20 17:30:33 +0530

    Upadet in code

--- Important Problem/Highest_character_Frequency.java
+++ Important Problem/Highest_character_Frequency.java
@@ -9,6 +9,7 @@ public class Highest_character_Frequency {
         String str = scn.next();
 
         int[] farr = new int[26]; // 0 pr a ki freq, 1 pr b ki freq..
+
         for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
             int idx = ch - 'a';
@@ -29,3 +30,13 @@ public class Highest_character_Frequency {
 
     }
 }
+
+
+/*
+ sample input
+ aaabbbbcccccccd
+
+ output:
+ c (Max times aaya hai c )
+
+*/
